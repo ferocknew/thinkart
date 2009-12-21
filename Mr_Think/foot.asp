9fc2754e1a2620b66e31fd3cc77eca2e55612bda
ferock@gmail.com <ferock@gmail.com@22d28e16-dd03-11de-b4bf-3fb2a63344f9>
2009-12-21 02:52:33 +0000

    调整文件结构

--- Mr_Think/foot.asp
+++
@@ -1,5 +0,0 @@
-</html>
-<%
-Session(CookieName&"_LastDo") = "" '最近的一次数据库操作
-Call CloseDB()
-%>
--- Mr_Think/function/default.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-	//网站基本信息
-    $.getJSON(file_url,{"code":"json"}, loaddoc);
-
-});
-function loaddoc(json){document.title=json.info.site_name;}
