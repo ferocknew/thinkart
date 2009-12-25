8e1a817f3b5a3d455ac5392abe17613904e551ef
ferock@gmail.com <ferock@gmail.com@22d28e16-dd03-11de-b4bf-3fb2a63344f9>
2009-12-25 11:35:03 +0000

    增加版本号常量，增加项目内容页面

---
+++ object/DW_www/DW_fang/TK-Admin/lib/header_html.asp
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="UTF-8">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="Content-Language" content="UTF-8" />
+<meta name="robots" content="all" />
+<meta name="generator" content="mrThink" />
+<script type="text/javascript" src="../lib/js/jquery.min.js"></script>
+<script type="text/javascript" src="../lib/js/js_jonah.js"></script>
+<script language="javascript" src="../lib/js/jquery.hiAlerts-min.js"></script>
+<script type="text/javascript" src="../lib/js/xheditor/xheditor-zh-cn.js"></script>
+<script type="text/javascript" src="../lib/js/default_lib.js"></script>
+<script type="text/javascript" src="lib/js/lib_admin.js"></script>
+<script type="text/javascript" src="lib/js/default_admin.js"></script>
+<link href="../lib/css/jquery.hiAlerts.css" rel="stylesheet" type="text/css" />
+<link href="files/css/style.css" rel="stylesheet" type="text/css" />
+<title>Mr.Thinker - Website Manager</title>
+</head>
+<body>
---
+++ object/DW_www/DW_fang/TK-Admin/lib/js/lib_admin.js
@@ -0,0 +1,46 @@
+/*
+ * classManage.asp
+ */
+//显示class列表
+function show_class_list(com_words){
+	var do_words=com_words.do_words;
+	var class_type = com_words.class_type;
+	var type_=com_words.type_;
+	var get_info_url=com_words.url;
+	var get_json=com_words.json;
+
+    function ezshow(class_type, class_json){
+        class_type.clearAll();
+        $(class_json).each(function(i){
+            class_type.addOption(class_json[i].classname, class_json[i].id)
+        })
+        class_type.setSelectedIndex(0);
+    };
+    if (type_ == 1) {
+        ezshow(class_type, get_json);
+    }
+    else {
+        $.getJSON(get_info_url, {
+            "code": "json",
+            "act": do_words
+        }, function(json){
+            var class_json = json.class_list;
+            ezshow(class_type, class_json);
+        });
+    }
+};
+/*
+ * index.asp
+ */
+//显示信息
+function show_info(json){
+    $("#sitename").val(json.info.site_name);
+    $("#comnam").val(json.info.com_name);
+    $("#leader").val(json.info.site_leader);
+    $("#siteicp").val(json.info.site_icp);
+    $("#siteurl").val(json.info.site_url);
+    $("#site_logo").val(json.info.site_logo);
+    $("#com_intro").val(json.info.site_com_intro);
+    show_edit($("#com_intro"));
+    loaddoc(json);
+};
